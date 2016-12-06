f563ea30fab2dfd2e01c5b82d5de646920574de9
xauel <xauel@mail.ru>
2016-12-06 17:50:07 +0600

    06.12.2016

---
+++ sxd/backup/sj_alaqan.job.php
@@ -0,0 +1,20 @@
+<?php
+$JOB = array (
+  'type' => 'backup',
+  'db' => 'alaqan',
+  'charset' => '0',
+  'zip' => '7',
+  'comment' => '',
+  'del_time' => '',
+  'del_count' => '',
+  'obj' => 
+  array (
+    'TA' => 
+    array (
+      0 => '*',
+    ),
+  ),
+  'job' => 'alaqan',
+  'title' => '15',
+);
+?>
